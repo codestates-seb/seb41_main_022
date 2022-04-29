56677fe71be99e833362c44ac9e3b6fab6c7316f
gimseyun <seyoonkim48@gmail.com>
2022-04-30 01:57:46 +0900

    calculator

---
+++ src/com/codestates/Main.java
@@ -0,0 +1,19 @@
+package com.codestates;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("===Java Calculator===");
+
+        Scanner input = new Scanner(System.in);
+        /*
+            요구 사항에 따라 간단한 계산기를 만들어주세요.
+            1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
+            2. 연산자의 종류는 +, -, *, / 입니다.
+            3. 소수점 연산을 수행할 수 있어야 합니다.
+            4. 연산 결과를 콘솔에 출력합니다.
+        */
+        // TODO...
+    }
+}
