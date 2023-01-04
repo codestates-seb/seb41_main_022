8e60b7ca62f613f62c11a559b8c1520ced6f0a3d
lisia004 <chkei0405@gmail.com>
2023-01-04 23:17:01 +0900

    시작

---
+++ server/src/main/java/codestates/main22/Main22Application.java
@@ -0,0 +1,13 @@
+package codestates.main22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main22Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/codestates/main22/Main22ApplicationTests.java
@@ -0,0 +1,13 @@
+package codestates.main22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Main22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
