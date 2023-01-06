ec4aa25c4af4b81394f73a61e1c6e2d2d9fdd5b8
lisia004 <chkei0405@gmail.com>
2023-01-06 15:21:58 +0900

    feat #2 : 기초 Controller, Dto, service, mapper, entity, repository 작성

--- server/src/main/java/codestates/main22/study/service/StudyService.java
+++ server/src/main/java/codestates/main22/study/service/StudyService.java
@@ -20,25 +20,25 @@ public class StudyService {
         return studyRepository.save(study);
     }
 
-//    public Study updateStudy(Study study) {
-//        Study findStudy = VerifiedStudy(study.getStudyId());
-//        return studyRepository.save(study);
-//    }
-//
-//    public Study findStudy(long studyId) {
-//        Study study = VerifiedStudy(studyId);
-//        return study;
-//    }
-//
-//    public Page<Study> findStudies(int page, int size) {
-//        return studyRepository.findAll(PageRequest.of(page, size, Sort.by("studyId").descending()));
-//    }
-//
-//    public void deleteStudy(long studyId) {studyRepository.deleteById(studyId);}
-//
-//    public Study VerifiedStudy(long studyId) {
-//        Optional<Study> optionalStudy = studyRepository.findById(studyId);
-//        Study findStudy = optionalStudy.orElseThrow(() -> new BusinessLogicException(ExceptionCode.STUDY_NOT_FOUND));
-//        return findStudy;
-//    }
+    public Study updateStudy(Study study) {
+        Study findStudy = VerifiedStudy(study.getStudyId());
+        return studyRepository.save(study);
+    }
+
+    public Study findStudy(long studyId) {
+        Study study = VerifiedStudy(studyId);
+        return study;
+    }
+
+    public Page<Study> findStudies(int page, int size) {
+        return studyRepository.findAll(PageRequest.of(page, size, Sort.by("studyId").descending()));
+    }
+
+    public void deleteStudy(long studyId) {studyRepository.deleteById(studyId);}
+
+    public Study VerifiedStudy(long studyId) {
+        Optional<Study> optionalStudy = studyRepository.findById(studyId);
+        Study findStudy = optionalStudy.orElseThrow(() -> new BusinessLogicException(ExceptionCode.STUDY_NOT_FOUND));
+        return findStudy;
+    }
 }
