77c3ad86163cea74ae4957db1295831e89323a23
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-09 16:33:00 +0900

    api 통신 테스트

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Header = () => {
   axios
-    .get("ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080/message/1")
+    .get("http://ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080/message/1")
     .then((res) => console.log(res.data));
 
   return <div></div>;
