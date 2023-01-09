395833ee50d1ee119bc63e6b7df72a77cce9bbee
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-09 11:40:05 +0900

    fix : global style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import "./global.css";
+import "./reset.css";
+import styled from "styled-components";
 function App() {
+  const H2 = styled.h2`
+    color: var(--mopo-10);
+  `;
   return (
     <div>
-      <h2>DITTO</h2>
+      <H2>DITTO</H2>
       <p>안녕하세용</p>
       <p>방가워용</p>
     </div>
