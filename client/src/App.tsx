d76273cda901b10a87754812d679cb19b8625485
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-09 15:36:32 +0900

    Feat/khw/global (#23)
    * fix : global style
    
    * fix : global style
    
    * feat - #4 공용초기설정

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./global.css";
+import "./reset.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import StudyHallPage from "./pages/StudyHallPage";
+import UserPage from "./pages/UserPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/study-hall" element={<StudyHallPage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
