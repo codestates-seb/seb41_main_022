5ef25dbe82ed9a713c37e8e617f23e821948a6d0
kimhw7 <khw786678@gmail.com>
2023-01-09 20:46:38 +0900

    feat - #28 : Header 컴포넌트 뷰구현완료
    Header 컴포넌트 로그인 시 뷰 구현 완료, 로그인/로그아웃 뷰 변화는 추후 완성예정

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,94 @@
-import axios from "axios";
+import styled from "styled-components";
+
+import Bell from "../assets/Bell.svg";
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 64px;
+  background-color: var(--green);
+  padding: 0 30px 0 30px;
+
+  > .header-logo {
+    font-family: "NovaCut";
+    color: var(--logo);
+    font-size: 50px;
+    letter-spacing: -6%;
+    cursor: pointer;
+    > span {
+      color: var(--red-00);
+    }
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 50px;
+    height: 50px;
+    border-radius: var(--radius-10);
+    margin-right: 8px;
+    cursor: pointer;
+  }
+  > .imgWrapper {
+    width: 50px;
+    height: 50px;
+    border-radius: var(--radius-10);
+    margin-right: 8px;
+    background-color: white;
+    > img {
+      width: 50px;
+      height: 50px;
+      border-radius: var(--radius-10);
+      cursor: pointer;
+    }
+  }
+  > .bell {
+    width: 28px;
+    height: 28px;
+    margin-right: 8px;
+    cursor: pointer;
+  }
+`;
+
+// 마이 스터디, 로그인, 로그아웃 버튼
+const WhiteButton = styled.button`
+  font-size: 14px;
+  background-color: var(--gray-10);
+  border-radius: var(--radius-20);
+  margin-right: 8px;
+  height: 28px;
+  border: 1px solid var(--green);
+  cursor: pointer;
+
+  :hover {
+    background-color: var(--green);
+    color: var(--gray-10);
+    border: 1px solid var(--gray-10);
+  }
+`;
 
 const Header = () => {
-  axios
-    .get("http://ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080/message/1")
-    .then((res) => console.log(res.data));
-
-  return <div></div>;
+  return (
+    <>
+      <HeaderWrapper>
+        <div className="header-logo">
+          Stu<span>d</span>y Tree
+        </div>
+        <ItemWrapper>
+          <div className="imgWrapper">
+            <img src="https://mystickermania.com/cdn/stickers/cartoons/pokemon-ditto-you-can-be-anything-512x512.png" />
+          </div>
+          <img className="bell" src={Bell} />
+          <WhiteButton>My Study</WhiteButton>
+          <WhiteButton>Log out</WhiteButton>
+        </ItemWrapper>
+      </HeaderWrapper>
+    </>
+  );
 };
 
 export default Header;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -5,8 +5,8 @@ import Header from "../components/Header";
 const HomePage = () => {
   return (
     <>
-      <div>HomePage</div>
       <Header />
+      <div>HomePage</div>
     </>
   );
 };
