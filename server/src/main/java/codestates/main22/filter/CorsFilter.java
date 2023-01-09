d8182f9086d7e0b6ddbe14bc7e3130a75b9d85e6
lisia004 <chkei0405@gmail.com>
2023-01-09 16:28:14 +0900

    feat #21 : cors 에러 확인 필터

---
+++ server/src/main/java/codestates/main22/filter/CorsFilter.java
@@ -0,0 +1,51 @@
+package codestates.main22.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods","POST");
+        response.addHeader("Access-Control-Allow-Methods", "PATCH");
+        response.addHeader("Access-Control-Allow-Methods", "GET");
+        response.addHeader("Access-Control-Allow-Methods", "DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Refresh");
+
+
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- server/src/main/java/codestates/main22/study/controller/StudyController.java
+++ server/src/main/java/codestates/main22/study/controller/StudyController.java
@@ -3,10 +3,11 @@ package codestates.main22.study.controller;
 import codestates.main22.dto.MultiResponseDto;
 import codestates.main22.dto.SingleResponseDto;
 import codestates.main22.study.dto.StudyDto;
+import codestates.main22.study.dto.StudyNotificationDto;
+import codestates.main22.study.dto.StudyRequesterDto;
 import codestates.main22.study.entity.Study;
 import codestates.main22.study.mapper.StudyMapper;
 import codestates.main22.study.service.StudyService;
-import codestates.main22.tree.dto.TreeDto;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/study")
+@RequestMapping("/study")
 @Validated
 public class StudyController {
     private final StudyService studyService;
@@ -29,7 +30,7 @@ public class StudyController {
         this.studyMapper = mapper;
     }
 
-    @PostMapping
+    @PostMapping //스터디 작성
     public ResponseEntity postStudy(@Valid @RequestBody StudyDto.Post requestBody) {
         Study study = studyMapper.studyPostDtoToStudy(requestBody);
         Study createStudy = studyService.createStudy(study);
@@ -37,7 +38,7 @@ public class StudyController {
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{study-id}")
+    @PatchMapping("/{study-id}") //스터디 수정
     public ResponseEntity patchStudy(@PathVariable("study-id") @Positive long studyId,
                                      @Valid @RequestBody StudyDto.Patch requestBody) {
         requestBody.setStudyId(studyId);
@@ -47,14 +48,14 @@ public class StudyController {
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 
-    @GetMapping("/{study-id}")
+    @GetMapping("/{study-id}") //특정 스터디 조회
     public ResponseEntity getStudy(@PathVariable("study-id") @Positive long studyId) {
         Study findStudy = studyService.findStudy(studyId);
         StudyDto.Response response = studyMapper.studyToStudyResponseDto(findStudy);
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 
-    @GetMapping
+    @GetMapping //스터디 전체 조회
     public ResponseEntity getStudies(@RequestParam int page,
                                      @RequestParam int size) {
         Page<Study> pageStudies = studyService.findStudies(page-1, size);
@@ -63,9 +64,33 @@ public class StudyController {
                 new MultiResponseDto<>(studyMapper.studiesToStudyResponseDto(studies), pageStudies), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{study-id}")
+    @DeleteMapping("/{study-id}") //스터디 삭제
     public ResponseEntity deleteStudy(@PathVariable("study-id") @Positive long studyId) {
         studyService.deleteStudy(studyId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{study-id}/notification") //studyHall/Notification에서 공지만 확인
+    public ResponseEntity getNotification(@PathVariable("study-id") @Positive long studyId) {
+        Study findStudy = studyService.findStudy(studyId);
+        StudyNotificationDto.Response response = studyMapper.studyToStudyNotificationResponseDto(findStudy);
+        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{study-id}/notification") //studyHall/Notification에서 공지만 수정
+    public ResponseEntity patchNotification(@PathVariable("study-id") @Positive long studyId,
+                                            @Valid @RequestBody StudyNotificationDto.Patch requestBody) {
+        requestBody.setStudyId(studyId);
+        Study study = studyMapper.studyNotificationPatchDtoToStudyNotification(requestBody);
+        Study updateStudyNotification = studyService.updateStudy(study);
+        StudyNotificationDto.Response response = studyMapper.studyToStudyNotificationResponseDto(updateStudyNotification);
+        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+    }
+
+    @GetMapping("/{study-id}/requester") //study 신청자만 보기
+    public ResponseEntity getRequester(@PathVariable("study-id") @Positive long studyId) {
+        Study findStudy = studyService.findStudy(studyId);
+        StudyRequesterDto.Response response = studyMapper.studyToStudyRequesterResponseDto(findStudy);
+        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 }
