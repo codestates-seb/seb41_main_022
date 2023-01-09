f3ba3788c7b448366ea2d1fb7c5aedd2dc7d5d2e
kimhw7 <khw786678@gmail.com>
2023-01-09 16:14:45 +0900

    api 통신 테스트

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const  HomePage = ()=> {
-    return <div>HomePage</div>;
-}
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <Header />
+    </>
+  );
+};
 
 export default HomePage;
