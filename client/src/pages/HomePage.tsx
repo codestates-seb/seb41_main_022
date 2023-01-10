34333b46cb7f8d75e372b074381b7ace3d9d8252
kimhw7 <100066239+kimhw7@users.noreply.github.com>
2023-01-10 14:22:54 +0900

    Feat/khw/card (#48)
    * feat - #28 : Header 컴포넌트 뷰구현완료
    
    Header 컴포넌트 로그인 시 뷰 구현 완료, 로그인/로그아웃 뷰 변화는 추후 완성예정
    
    * feat #38 모집카드 컴포넌드 생성
    
    모집 개별 카드 뷰 구현
    
    * feat #28 header app.tsx에 추가

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 import Header from "../components/Header";
 
 const HomePage = () => {
   return (
     <>
-      <Header />
-      <div>HomePage</div>
+      <HomepageWrapper>
+        <Header />
+        <div className="test">HomePage</div>
+      </HomepageWrapper>
     </>
   );
 };
+
+// 전체 감싸는 태그
+const HomepageWrapper = styled.div`
+  background-color: var(--green);
+  > .test {
+    color: white;
+  }
+`;
 
 export default HomePage;
