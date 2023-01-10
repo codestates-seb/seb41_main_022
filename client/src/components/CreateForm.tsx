a9aa6a67b7dd6a69716880123b8df1a37d2426e7
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-10 13:53:16 +0900

    feat #31 : CreateForm 뷰 (#37)

--- client/src/components/CreateForm.tsx
+++ client/src/components/CreateForm.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import WeekBar from "./WeekBar";
+import Toggle from "./Toggle";
 
 const CreateForm = () => {
   const { register, handleSubmit } = useForm();
@@ -13,8 +14,41 @@ const CreateForm = () => {
           <input id="title" type="text" {...register("title")} />
           <label htmlFor="text">한 줄 설명</label>
           <input id="text" type="text" {...register("text")} />
-          <button type="submit">로그인</button>
-          <WeekBar />
+          <div>Tags</div>
+          <div>
+            <WeekBar />
+          </div>
+          <div>
+            인원 :{" "}
+            <input
+              className="person"
+              id="person"
+              type="number"
+              {...register("person")}
+              min="1"
+              placeholder="1"
+            />
+          </div>
+          <div>
+            <div className="toggleBox">
+              Online
+              <Toggle />
+            </div>
+            <div className="toggleBox">
+              Public
+              <Toggle />
+            </div>
+          </div>
+          <div>
+            <StartDateButton>시작 날짜</StartDateButton>
+          </div>
+          <label htmlFor="content">내용</label>
+          <div>
+            <textarea id="content" {...register("content")} />
+          </div>
+          <div>
+            <RedButton type="submit">Create Study</RedButton>
+          </div>
         </Form>
       </ContentDiv>
     </Main>
@@ -29,6 +63,9 @@ const Main = styled.main`
   background-color: var(--beige-00);
   * {
     font-family: "mainB", Arial;
+  }
+  button {
+    border-style: none;
   }
 `;
 //내뷰 480px
@@ -50,5 +87,42 @@ const Form = styled.form`
   > input {
     margin: 5px 0 20px;
     font-family: "mainM", Arial;
+  }
+  > div {
+    margin: 5px 0 20px;
+    display: flex;
+    align-items: center;
+  }
+  .person {
+    margin-left: 8px;
+    width: 30px;
+  }
+  .toggleBox {
+    font-size: 12px;
+    margin-right: 25px;
+  }
+`;
+const StartDateButton = styled.button`
+  background-color: #dfe4e0;
+  width: 200px;
+  font-family: "mainM", Arial;
+  border: 1px solid var(red);
+  border-radius: var(--radius-10);
+  padding: 6px;
+  :hover {
+    border: 1px solid var(--green-00);
+    padding: 5px;
+  }
+`;
+const RedButton = styled.button`
+  width: 240px;
+  padding: 3px;
+  background-color: var(--red-00);
+  color: var(--beige-00);
+  border-radius: var(--radius-30);
+  font-family: "mainL", Arial;
+  font-size: 22px;
+  :hover {
+    background-color: var(--red-10);
   }
 `;
