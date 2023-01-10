8c38a4094be96e9c0c154b5251e6965191800888
kimhw7 <khw786678@gmail.com>
2023-01-10 15:54:28 +0900

    feat #47 모집카드 wrapper 구현
    반응형 정렬, 기본 정렬 완성

---
+++ client/src/components/homepage/RecruitmentList.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import Data from "../../util/dummyData";
+import Recruitment from "./Recruitment";
+
+const RecruitmentList = () => {
+  return (
+    <RecruitmentWrapper>
+      {Data.data.map((el) => (
+        <Recruitment
+          key={el.studyId}
+          teamName={el.teamName}
+          summary={el.summary}
+        />
+      ))}
+    </RecruitmentWrapper>
+  );
+};
+
+const RecruitmentWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  height: auto;
+
+  //모바일
+  @media screen and (max-width: 768px) {
+    width: 321px;
+  }
+  //태블릿
+  @media screen and (min-width: 768px) and (max-width: 1200px) {
+    width: 750px;
+  }
+  // 웹
+  @media screen and (min-width: 1200px) {
+    width: 1100px;
+  }
+`;
+
+export default RecruitmentList;
