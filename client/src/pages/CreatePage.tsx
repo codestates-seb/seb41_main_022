7bbc0f2b3bcca2a38a16f6f57bde46b696d7e0c8
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-11 13:18:40 +0900

    refactor #31 : CreateForm (#62)

--- client/src/pages/CreatePage.tsx
+++ client/src/pages/CreatePage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import CreateForm from "../components/CreateForm";
+import styled from "styled-components";
 
-const  CreatePage = ()=> {
-    return <div>CreatePage</div>;
-}
+const CreatePage = () => {
+  return (
+    <CreateFormDiv>
+      <CreateForm />
+    </CreateFormDiv>
+  );
+};
 
 export default CreatePage;
+const CreateFormDiv = styled.div`
+  background-color: var(--green);
+  height: auto;
+  width: 100%;
+  padding: 100px 0;
+`;
