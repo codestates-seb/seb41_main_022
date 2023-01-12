bce6b617a356c9251793af82a522912e20a31905
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-12 14:31:39 +0900

    feat #42 calendar get post 기능 추가

--- client/src/components/StudyHallPage/StudyHallTopNav.tsx
+++ client/src/components/StudyHallPage/StudyHallTopNav.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import StudyHallRightNav from "./StudyHallRightNav";
+import StudyHallRightNav from "./notification/StudyHallRightNav";
 
 const StudyHallTopNav = () => {
   return (
--- client/src/pages/StudyHallPage.tsx
+++ client/src/pages/StudyHallPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import StudyHallHead from "../components/StudyHallPage/StudyHallHead";
 import StudyHallTopNav from "../components/StudyHallPage/StudyHallTopNav";
-import StudyHallNotification from "../components/StudyHallPage/StudyHallNotification";
+import StudyHallNotification from "../components/StudyHallPage/notification/StudyHallNotification";
 import Community from "../components/StudyHallPage/community/Community";
 
 const StudyHallPage = () => {
