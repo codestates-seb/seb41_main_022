58a947f186c049ee2a94312bf555a04c9840f86f
vinyangda <110925532+vinyangda@users.noreply.github.com>
2023-01-12 17:12:58 +0900

    feat -#40 : Study hall -main 중간저장 (#72)

--- client/src/components/StudyHallPage/StudyHallMain.tsx
+++ client/src/components/StudyHallPage/StudyHallMain.tsx
@@ -1,4 +1,41 @@
+import { el } from "@fullcalendar/core/internal-common";
+import styled, { StyleSheetManager } from "styled-components";
+
+//컴포넌트 import
+import StudyInfo from "./Main/StudyInfo";
+import CreateComment from "./Main/CreateComment";
+import Comments from "./Main/Comments";
+import Answer from "./Main/Answer";
+
+//API Data import
+import StudyHallData from "../../util/dummyDataStudyHall";
+import { CommentsData } from "../../util/dummyDataStudyHall";
 const StudyHallMain = () => {
-  return <div></div>;
+  return (
+    <MainWrapper>
+      <div className="padding">
+        <div>
+          {
+            <StudyInfo
+              teamName={StudyHallData.data.teamName}
+              summary={StudyHallData.data.summary}
+              content={StudyHallData.data.content}
+            />
+          }
+          <CreateComment />
+          {CommentsData.data.map((el) => (
+            <Comments
+              key={el.chatId}
+              chatUserId={el.chatUserId}
+              content={el.content}
+              answers={el.answers}
+            />
+          ))}
+        </div>
+      </div>
+    </MainWrapper>
+  );
 };
 export default StudyHallMain;
+
+const MainWrapper = styled.div``;
