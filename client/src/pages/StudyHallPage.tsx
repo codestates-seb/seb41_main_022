b5251e2b7f49fb8f545c71c54d21976cba56cca5
kimhw7 <100066239+kimhw7@users.noreply.github.com>
2023-01-13 13:21:50 +0900

    feat #43 스터디홀 - setting 생성 (#79)
    가입요청 렌더링 가능, 버튼 구현

--- client/src/pages/StudyHallPage.tsx
+++ client/src/pages/StudyHallPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
+import Setting from "../components/StudyHallPage/setting/Setting";
 import StudyHallHead from "../components/StudyHallPage/StudyHallHead";
 import StudyHallTopNav from "../components/StudyHallPage/StudyHallTopNav";
 import StudyHallNotification from "../components/StudyHallPage/notification/StudyHallNotification";
@@ -16,7 +18,7 @@ const StudyHallPage = () => {
       {page === "main" && <StudyHallMain />}
       {page === "community" && <Community />}
       {page === "calendar" && <div></div>}
-      {page === "setting" && <div></div>}
+      {page === "setting" && <Setting />}
     </div>
   );
 };
---
+++ client/src/util/data/dummydataSetting.ts
@@ -0,0 +1,21 @@
+const ApplicationData = {
+  data: [
+    {
+      userId: 1,
+      username: "vinyangda",
+      imgUrl: "https://avatars.githubusercontent.com/u/110925532?s=64&v=4",
+    },
+    {
+      userId: 2,
+      username: "kimhw7",
+      imgUrl: "https://avatars.githubusercontent.com/u/100066239?s=64&v=4",
+    },
+    {
+      userId: 3,
+      username: "leewang31",
+      imgUrl: "https://avatars.githubusercontent.com/u/99003659?s=64&v=4",
+    },
+  ],
+};
+
+export default ApplicationData;
