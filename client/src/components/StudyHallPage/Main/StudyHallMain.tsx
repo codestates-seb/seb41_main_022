851b4153bed5ef95e8edf52855fb12c3c7c2a9dd
vinyangda <110925532+vinyangda@users.noreply.github.com>
2023-01-19 09:42:02 +0900

    Feat/ydv/main (#124)
    * fix -#40: comment error fix hahahaha
    
    * refactor -#40 : dummy에서 API 초기통신으로 받아왔습니다

--- client/src/components/StudyHallPage/Main/StudyHallMain.tsx
+++ client/src/components/StudyHallPage/Main/StudyHallMain.tsx
@@ -1,38 +1,67 @@
 import styled, { StyleSheetManager } from "styled-components";
 import { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import { useParams } from "react-router-dom";
 
 import StudyInfo from "./StudyInfo";
 import CreateComment from "./CreateComment";
 import Comments from "./Comments";
 import Pagination from "../../Pagination";
-
-//API Data import
 import { CommentsData } from "../../../util/dummyDataStudyHall";
+interface Studies {
+  chatId: number;
+  username: string;
+  content: string;
+  answers: [];
+  pageInfo: any;
+  totalElements: number;
+}
+interface Data {
+  data: any;
+}
 
 const StudyHallMain = () => {
-  // const [page, setPage] = useState(1);
-  // const [totalPages, setTotalPages] = useState(10);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(10);
+  const { studyId, i, limit } = useParams();
+  //데이터 요청
+  const [commentsData, setCommentsData] = useState<Studies[] | undefined>();
+  const getCommentsData = (url: string): Promise<AxiosResponse<Data>> => {
+    return axios.get(url, {
+      headers: {
+        "access-Token": "abcd",
+      },
+    });
+  };
 
-  // const handlePrevPage = (prevPage: number) => {
-  //   setPage((prevPage) => prevPage - 1);
-  // };
+  const handlePrevPage = (prevPage: number) => {
+    setPage((prevPage) => prevPage - 1);
+  };
 
-  // const handleNextPage = (nextPage: number) => {
-  //   setPage((nextPage) => nextPage + 1);
-  // };
+  const handleNextPage = (nextPage: number) => {
+    setPage((nextPage) => nextPage + 1);
+  };
 
-  // const fetchData = async () => {
-  //   const response =
-  //     await fetch(`https://api.coingecko.com/api/v3/coins/markets?
-  //   vs_currency=usd&order=market_cap_desc&?${page}&per_page=10&sparkline=false`);
-  //   const result = await response.json();
+  const fetchData = async () => {
+    const response = await fetch(
+      `http://ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080//chat/${studyId}?page=${i}&size=${limit}`
+    );
+    const result = await response.json();
+    setTotalPages(totalPages);
+  };
 
-  //   setTotalPages(totalPages);
-  // };
+  // API를 받아서 코멘트로 쏴준다
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    getCommentsData(
+      `http://ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080/chat/${studyId}?page=${1}&size=${15}`
+    ).then((res) => {
+      setCommentsData(res.data.data);
+    });
+  });
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <MainWrapper>
@@ -40,20 +69,21 @@ const StudyHallMain = () => {
         <div>
           {<StudyInfo />}
           <CreateComment />
-          {CommentsData.data.map((el) => (
-            <Comments
-              key={el.chatId}
-              chatUserId={el.chatUserId}
-              content={el.content}
-              answers={el.answers}
-              totalElements={CommentsData.pageInfo.totalElements}
-            />
-          ))}
+          {commentsData &&
+            commentsData.map((el) => (
+              <Comments
+                key={el.chatId}
+                chatUserId={el.username}
+                content={el.content}
+                answers={el.answers}
+                totalElements={el.pageInfo.totalElements}
+              />
+            ))}
           {/* <Pagination
-            totalPages={totalPages}
-            currentPage={page}
-            handlePrevPage={handlePrevPage}
-            handleNextPage={handleNextPage}
+            // total={totalQuestions}
+            limit={limit}
+            page={page}
+            setPage={setPage}
           /> */}
         </div>
       </div>
