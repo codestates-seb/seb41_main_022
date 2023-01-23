4b6cec5299aae4ee89167aa445cf6fc05dcdaf38
Leewang31 <kiwan1111@gmail.com>
2023-01-23 19:57:59 +0900

    feat #42 공지 리로ë드 삭제

--- client/src/components/StudyHallPage/StudyHallRightNav.tsx
+++ client/src/components/StudyHallPage/StudyHallRightNav.tsx
@@ -14,7 +14,6 @@ const StudyHallRightNav = () => {
   const { studyId } = useParams();
   const [noticeR, setNoticeR] = useState("");
   useEffect(() => {
-    // if()
     if (zustandStudyId !== studyId) {
       resetNotice();
     }
