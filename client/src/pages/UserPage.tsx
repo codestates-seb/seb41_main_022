e5f62501760de4f9f3d8509c94aa958b10deec5c
kimhw7 <100066239+kimhw7@users.noreply.github.com>
2023-01-24 19:40:11 +0900

    회원탈퇴 구현 (#170)
    * feat #38 모집카드 online/offline 표시
    
    색변경해서 구분
    
    * feat #31 토글 방향 변경
    
    기본 방향이 왼쪽이 되도록 변경
    
    * feat #46 회원탈퇴 구현중
    
    회원탈퇴 버튼 클릭시 안내문구 출력
    
    * feat #46 회원탈퇴 구현
    
    동의사항 읽은 후 체크 해야지만 회원 탈퇴 가능

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import axios from "axios";
 import styled from "styled-components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 //구분선
 import Profile from "../components/userpage/Profile";
 import MyStudy from "../components/userpage/MyStudy";
 import MyStudyList from "../components/userpage/MyStudyList";
+import { useCookies } from "react-cookie";
 
 const UserPage = () => {
+  const [cookies] = useCookies(["token"]);
+  const [isOpenAgreePage, setIsOpenAgreePage] = useState(false);
+  const [agreeWithdraw, setAgreeWithdraw] = useState(false);
+  const navigate = useNavigate();
+
+  const withdraw = () => {
+    if (agreeWithdraw) {
+      axios
+        .delete(
+          "http://ec2-13-209-56-72.ap-northeast-2.compute.amazonaws.com:8080/user",
+          {
+            headers: {
+              "access-Token": cookies.token.accessToken,
+              "refresh-Token": cookies.token.refreshToken,
+            },
+          }
+        )
+        .then(() => navigate("/"));
+    }
+  };
+
+  const handleOpenWithdraw = () => {
+    setIsOpenAgreePage(!isOpenAgreePage);
+  };
+
+  const handleActiveWithdrawButton = () => {
+    setAgreeWithdraw(!agreeWithdraw);
+  };
   return (
     <Main>
       <Container>
@@ -14,9 +45,51 @@ const UserPage = () => {
         <MyStudyList />
         <h3 className="title">Tags</h3>
         <h3 className="title">나무나무</h3>
-        <button className="button" type="submit">
-          회원탈퇴
+        <button
+          onClick={handleOpenWithdraw}
+          className={isOpenAgreePage ? "button buttonActive" : "button"}
+          type="submit"
+        >
+          회원탈퇴 열기
         </button>
+        {isOpenAgreePage ? (
+          <WithdrawAgree>
+            <div className="textZone">
+              <p>회원탈퇴 안내사항</p>
+              <ul>
+                <li>
+                  • 회원탈퇴가 진행된 후에는 다시 되돌릴 수 없습니다. 아래
+                  내용을 잘 읽어보신 후 회원탈퇴를 진행해 주시기 바랍니다.
+                </li>
+                <li>
+                  • 회원탈퇴 후에는 스터디장으로서 운영하시던 스터디의 다음
+                  스터디원에게 자동으로 스터디장 위임이 됩니다. 원하시는
+                  인원에게 스터디장 위임을 하시려면 스터디홀 - setting
+                  페이지에서 스터디장 위임을 진행해주세요.
+                </li>
+                <li>
+                  • 회원탈퇴 후에도 존재하는 스터디는 삭제되지 않으며, 스터디의
+                  데이터는 웹사이트에 남게 됩니다. 스터디 삭제를 원하시면
+                  스터디홀 - setting 페이지에서 스터디 삭제를 진행해주세요.
+                </li>
+              </ul>
+              <div className="checkboxWrapper">
+                <p>위 내용을 확인했으며, 회원 탈퇴에 동의합니다.</p>
+                <input type={"checkbox"} onClick={handleActiveWithdrawButton} />
+              </div>
+            </div>
+            <div className="buttonWrapper">
+              <WithdrawButton
+                onClick={withdraw}
+                className={
+                  agreeWithdraw ? "withdrawButton active" : "withdrawButton"
+                }
+              >
+                회원탈퇴
+              </WithdrawButton>
+            </div>
+          </WithdrawAgree>
+        ) : null}
       </Container>
     </Main>
   );
@@ -36,7 +109,7 @@ const Main = styled.div`
 const Container = styled.div`
   background-color: var(--beige-00);
   width: 1024px;
-  height: 183px;
+  height: 1100px;
   margin: 0px auto;
 
   //My study, Tag 등 각 title
@@ -63,6 +136,49 @@ const Container = styled.div`
       transition-duration: 0.5s;
     }
   }
+
+  > .buttonActive {
+    background-color: var(--gray-20);
+  }
 `;
 
+const WithdrawAgree = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  width: 65%;
+  height: 340px;
+  margin-left: 120px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  background-color: white;
+  border: 1px orange solid;
+  > .buttonWrapper {
+    margin-top: 24px;
+    display: flex;
+    justify-content: end;
+    > .active {
+      background-color: var(--red-00);
+      color: var(--beige-00);
+      hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .checkboxWrapper {
+    p {
+      display: inline;
+    }
+  }
+`;
+
+const WithdrawButton = styled.button`
+  background-color: #976663;
+  color: var(--grey-00);
+  width: 80px;
+  padding: 4px 8px;
+  border-radius: var(--radius-10);
+  border: none;
+`;
 export default UserPage;
