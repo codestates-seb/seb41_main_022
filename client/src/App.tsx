91fbbedf61de1300799f2473180f53f85a492ef5
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-01-26 18:15:53 +0900

    feat #4 헤더 고정 (#216)
    * feat #4 헤더 고정
    
    * feat #4 헤더 에러 수정
    
    * feat #4 헤더 에러 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,23 +12,33 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
 import TokenPage from "./pages/TokenPage";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/edit/:studyId" element={<EditPage />} />
-        <Route path="/study-hall/:page/:studyId" element={<StudyHallPage />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/Token.html" element={<TokenPage />} />
-      </Routes>
+      <MainWrapper>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/edit/:studyId" element={<EditPage />} />
+          <Route
+            path="/study-hall/:page/:studyId"
+            element={<StudyHallPage />}
+          />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/Token.html" element={<TokenPage />} />
+        </Routes>
+      </MainWrapper>
       <Footer />
     </>
   );
 }
 
 export default App;
+
+const MainWrapper = styled.article`
+  margin-top: 64px;
+`;
