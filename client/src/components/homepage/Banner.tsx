ef597ebb5738933d400497bba4610cd9cafa95e1
vinyangda <110925532+vinyangda@users.noreply.github.com>
2023-01-28 14:57:47 +0900

    feat #38 : 배너css수정 (#240)

--- client/src/components/homepage/Banner.tsx
+++ client/src/components/homepage/Banner.tsx
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import React, { useRef, useEffect } from "react";
 
-import StudyTreeBackground from "../.././assets/StudyTreeBackground.svg";
+import first from "../.././assets/first.svg";
+import second from "../.././assets/second.svg";
+import third from "../.././assets/third.svg";
+
 const Banner = () => {
   const settings = {
     dots: true,
@@ -11,7 +15,7 @@ const Banner = () => {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 10000,
     pauseOnHover: true,
     arrows: false,
@@ -23,19 +27,36 @@ const Banner = () => {
           <div className="title">
             Stu<span>d</span>y Tree
           </div>
-          <img src={StudyTreeBackground} />
-        </BannerWrapper>
-        <BannerWrapper>
-          <div className="title">
-            Stu<span>d</span>y Tree
+          <div className="subtitle">
+            너와 나의 &nbsp; <span>스터디</span>
           </div>
-          <img src={StudyTreeBackground} />
+          <img src={first} />
         </BannerWrapper>
-        <BannerWrapper>
-          <div className="title">
-            Stu<span>d</span>y Tree
+        <BannerWrapper className="bannerWrapper">
+          <div className="moveCreate">
+            <span>스터디</span> 생성하기
           </div>
-          <img src={StudyTreeBackground} />
+          <div className="second-title">
+            <div className="s-first">나와 같은</div>
+            <div className="s-second">
+              <span>사람을</span> 모으고 싶다면?
+            </div>
+          </div>
+          <img src={second} />
+        </BannerWrapper>
+        <BannerWrapper className="bannerWrapper">
+          <div className="third-title">
+            <div className="t-first">
+              나와 <span>같은</span>
+            </div>
+            <div className="t-second">
+              사람이 있을까 <span>?</span>
+            </div>
+            <div className="moveStudies">
+              <span>스터디</span> 둘러보기
+            </div>
+            <img src={third} />
+          </div>
         </BannerWrapper>
       </Slider>
     </SlideWrapper>
@@ -43,32 +64,152 @@ const Banner = () => {
 };
 export default Banner;
 const SlideWrapper = styled.main`
-  width: 100%;
+  width: 100vw;
+  height: 100%;
   .bannerWrapper {
     display: flex;
     justify-content: center;
   }
 `;
 const BannerWrapper = styled.div`
-  width: 100%;
+  * {
+    font-family: "MainM";
+  }
+  width: 100vw;
+  height: 100vh;
   position: relative;
+  justify-content: center;
   img {
-    margin: 0 auto;
+    width: 100vw;
   }
   > .title {
     display: flex;
     font-family: "logo";
     color: var(--logo);
-    font-size: 180px;
+    font-size: 1125%;
     letter-spacing: -3px;
     position: absolute;
     width: 100%;
-    margin-top: 270px;
+    margin-top: 230px;
     text-align: center;
     justify-content: center;
     cursor: pointer;
     > span {
       color: var(--red-00);
+      font-family: "logo";
+    }
+  }
+
+  > .moveCreate {
+    font-size: 25px;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    margin-top: 400px;
+    margin-left: 150px;
+    color: var(--logo);
+    > span {
+      color: var(--red-00);
+    }
+  }
+
+  > .second-title {
+    display: flex;
+    color: var(--logo);
+    font-size: 100px;
+    letter-spacing: -3px;
+    position: absolute;
+    width: 100%;
+    margin-top: 400px;
+    padding-right: 50px;
+    text-align: right;
+    justify-content: right;
+    > .s-first {
+      position: absolute;
+    }
+    > .s-second {
+      display: flex;
+      margin-top: 100px;
+      cursor: pointer;
+      > span {
+        justify-content: flex;
+        display: flex;
+        color: var(--red-00);
+      }
+    }
+  }
+  > .third-title {
+    display: flex;
+    color: var(--logo);
+    font-size: 100px;
+    letter-spacing: -3px;
+    position: absolute;
+    width: 100%;
+    margin: 50px, 100px, 0px, 50px;
+    text-align: right;
+
+    > .t-first {
+      display: flex;
+      color: var(--logo);
+      font-size: 100px;
+      letter-spacing: -3px;
+      position: absolute;
+      width: 100%;
+      margin-top: 100px;
+      padding-right: 50px;
+
+      > span {
+        justify-content: flex;
+        display: flex;
+        color: var(--red-00);
+      }
+    }
+    > .moveStudies {
+      font-size: 25px;
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      margin-top: 450px;
+      margin-left: 950px;
+      color: var(--logo);
+      > span {
+        justify-content: flex;
+        display: flex;
+        color: var(--red-00);
+      }
+    }
+    > .t-second {
+      display: flex;
+      color: var(--logo);
+      font-size: 100px;
+      letter-spacing: -3px;
+      position: absolute;
+      width: 100%;
+      margin-top: 200px;
+      margin-right: 50px;
+      > span {
+        justify-content: flex;
+        display: flex;
+        color: var(--red-00);
+      }
+    }
+  }
+  .subtitle {
+    height: 100%;
+    width: 100%;
+    font-size: 20px;
+    display: flex;
+    position: absolute;
+    background-color: none;
+    width: 100%;
+    text-align: center;
+    justify-content: center;
+    margin-top: 450px;
+    color: var(--beige-10);
+
+    > span {
+      color: var(--red-00);
+      margin: 0px, 10px, 0px, 10px;
     }
   }
 `;
